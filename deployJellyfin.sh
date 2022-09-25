2bac45bbe5834b1c27742770f74d38cd99342323
kameshX007 <kameshbook@gmail.com>
2022-09-25 13:39:28 +0530

    #

--- deployJellyfin.sh
+++ deployJellyfin.sh
@@ -2,11 +2,11 @@
 #Checking whether username is passed to script
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployJellyfin.log && exit 0 || echo "Will be deploying container for user $dockerUser">>deployJellyfin.log;
+[ -z "$dockerUser" ] && echo "Please pass username...!!!" && exit 0 || echo "Will be deploying container for user $dockerUser";
 
 #Deploying docker compose
-echo "Deploying Jellyfin...">>deployJellyfin.log;
-DOCKER_USER=$dockerUser PLEX_CLAIM=$plexClaim docker-compose down
-DOCKER_USER=$dockerUser PLEX_CLAIM=$plexClaim docker-compose up -d
-echo "Jellyfin deployment successfull...">>deployJellyfin.log;
+echo "Deploying Jellyfin...";
+DOCKER_USER=$dockerUser docker-compose down
+DOCKER_USER=$dockerUser docker-compose up -d
+echo "Jellyfin deployment successfull...!!!";
 exit 0
